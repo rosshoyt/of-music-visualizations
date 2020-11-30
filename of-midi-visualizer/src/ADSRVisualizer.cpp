15ca51038cbdbe53365729846f38602fc97fc9d1
Ross Hoyt <rosshoytmusic@gmail.com>
2020-11-29 19:55:33 -0800

    Moved envelope implementation to EnvelopeSegment class

--- of-midi-visualizer/src/ADSRVisualizer.cpp
+++ of-midi-visualizer/src/ADSRVisualizer.cpp
@@ -1,7 +1,7 @@
 #include "ADSRVisualizer.h"
 
 //--------------------------------------------------------------
-ADSRVisualizer::ADSRVisualizer(std::string uid) : MIDIAnimationComponent(uid) {}
+ADSRVisualizer::ADSRVisualizer(std::string uid) : MIDIAnimationComponent(uid), attackSegment(EnvelopeSegmentType::ATTACK, 0, 1, 1, 0.25) {}
 
 //--------------------------------------------------------------
 void ADSRVisualizer::setup() {
@@ -13,15 +13,15 @@ void ADSRVisualizer::setup() {
 
 //--------------------------------------------------------------
 void ADSRVisualizer::setupGUI() {		
-	//gui.add(testADSRToggle.set("Test ADSR", false));
-	gui.add(splineIntensitySlider.set("Spline Intensity", 0, -.3, .3));
-	for (int i = 0; i < scXDefaults.size(); ++i) {
+	gui.add(attackSegment.splineIntensitySlider);
+	//gui.add(splineIntensitySlider.set("Spline Intensity", 0, -.3, .3));
+	/*for (int i = 0; i < scXDefaults.size(); ++i) {
 		auto pNumber = std::to_string(i + 1);
 		splineControlX.push_back(ofParameter<double>().set("x" + pNumber, scXDefaults[i], minF, maxF));
 		splineControlY.push_back(ofParameter<double>().set("y" + pNumber, scYDefaults[i], minF, maxF));
 		gui.add(splineControlX[i]);
 		gui.add(splineControlY[i]);
-	}
+	}*/
 	/*gui.add(splineControlX2.set("control point X", .5f, minF, maxF));
 	gui.add(splineControlY2.set("spline Amount", .75f, minF, maxF));*/
 	gui.add(changeSpeed.set("color speed ms", 300, 1, 5000));
@@ -50,85 +50,32 @@ bool ADSRVisualizer::shouldChange() {
 
 //--------------------------------------------------------------
 void ADSRVisualizer::draw() {
-
-	//gui.draw();
-	int maxVel = 0;
+	/*int maxVel = 0;
 	int numNotes = 0;
 	for (auto channel : midiPortState->getAllChannelNotes()) {
 		for (auto note : channel) {
 			maxVel = std::max(note.second, maxVel);
 			++numNotes;
 		}
-	}
-
-	tk::spline spline;
-	std::vector<double> xTemps, yTemps;
+	}*/
 
 	//update time-based params
 	if (shouldChange()) {
 		ofSetColor(utils::color::getRandomColor()); //colors[i % colors.size()]);
 		// update which y val to augment
-		if (currentYToChange > maxYToChange) {
+		/*if (currentYToChange > maxYToChange) {
 			currentYToChange = minYToChange;
 		}
 		else {
 			++currentYToChange;
-		}
-
-	}
-
-
-	int numExternalControlPointsPerSide = 1; // TODO rename - the points beyond the visible ADSR segment on each side
-	int numImmovablePointsPerSide = 1; // TODO rename - the points that can't be moved from where they are (start and end of visible ADSR segment)
-	int nonRegularPointsBuffer = numExternalControlPointsPerSide + numImmovablePointsPerSide;
-	auto size = splineControlX.size();
-	for (auto i = 0; i < size; i++) {
-		//if(i < size - 1)
-		auto xVal = splineControlX[i];
-		xTemps.push_back(xVal);
-
-		//if(xVal < 0)
-		auto yVal = splineControlY[i].get();
-
-		// TODO validation for when there aren't any external control poitns
-		if (i < numExternalControlPointsPerSide) {
-			yVal -= splineIntensitySlider;
-		}
-		else if (i < nonRegularPointsBuffer || i == size - nonRegularPointsBuffer) {
-
-		}
-		else if (i >= size - 1 - numExternalControlPointsPerSide) {
-			yVal -= splineIntensitySlider;
-		}
-		else {
-			yVal += splineIntensitySlider;
-		}/*
-		else if (i >= nonRegularPointsBuffer || i < size - nonRegularPointsBuffer || i == size - 1) {
-			yVal += splineIntensitySlider;
 		}*/
-
-		yTemps.push_back(yVal);
-		//yTemps.push_back(i == currentYToChange && numNotes > 0 ? splineControlY[i] * maxVel / 128.f : splineControlY[i]);
 	}
-
-	//std::sort(xTemps.begin(), xTemps.end());//, sortDouble);
-
-	spline.set_points(xTemps, yTemps);
-
-
-
-	/*spline.set_points({ scXDefaults[0], splineControlX[1], splineControlX[2], splineControlX[3], scXDefaults[4] },
-	{ splineControlY[0], splineControlY[1], splineControlY[2], splineControlY[3], splineControlY[4] }
-	);
-	*/
-	//float width = ofGetWidth(), height = ofGetHeight();
-
-
 
 	for (int i = 0; i < numPoints; ++i) {
 
 		double xF = float(i) / numPoints;
-		float heightScale = spline(xF);
+		float heightScale = attackSegment.getLevel(xF);
+		//std::cout << "Height scale = " << heightScale << '\n';
 
 		auto circleX = animationWidth * xF;
 		auto circleY = animationHeight - animationHeight * heightScale;
@@ -136,9 +83,5 @@ void ADSRVisualizer::draw() {
 
 		//std::cout << "Height Scale = " << heightScale << " for x = " << i << '\n';
 	}
-
-}
-
-void ADSRVisualizer::drawADSRTest() {
 
 }
--- of-midi-visualizer/src/ADSRVisualizer.h
+++ of-midi-visualizer/src/ADSRVisualizer.h
@@ -7,8 +7,92 @@
 #include <map>
 #include <algorithm>
 #include "ofxGui.h"
-#include "ADSR.h"
 
+enum EnvelopeSegmentType {
+	ATTACK, DECAY, SUSTAIN, RELEASE
+};
+
+class EnvelopeSegment {
+public:
+	EnvelopeSegment(EnvelopeSegmentType type, double start, double startLevel, double end, double endLevel) : type(type), start(start), startLevel(startLevel), end(end), endLevel(endLevel) {
+		init();
+	}
+
+	// Public GUI Sliders
+	ofParameter<float> splineIntensitySlider;
+
+	// TODO refactor - store calculated value to avoid calculating redundantly?
+	float getLevel(double xVal) {
+		tk::spline spline;
+		std::vector<double> xTemps(splineControlX), yTemps(splineControlY);
+
+		yTemps[0] -= splineIntensitySlider;
+		yTemps[2] += splineIntensitySlider;
+		yTemps[3] += splineIntensitySlider;
+ 		yTemps[5] -= splineIntensitySlider;
+
+		spline.set_points(xTemps, yTemps);
+
+		return spline(xVal);
+	}
+
+	double getTotalLength() {
+		return totalLength;
+	}
+
+	// TODO (only if implementation completed using real-time values for Segment time values)
+	//bool containsTime(long elapsedTimeMS) {}
+	//bool containsTime(double elapsedTimeMS) {}
+private:
+
+	void init() {
+		// set derived fields
+		totalLength = end - start;
+
+		// setup 6-point control spline in default state (linear)
+		double xStepSize = totalLength / NUM_INTERNAL_CONTROL_SEGMENTS;
+		double yStepSize = (endLevel - startLevel) / NUM_INTERNAL_CONTROL_SEGMENTS;
+		double yVal = startLevel - yStepSize;
+		double xVal = start - xStepSize;
+		for (int i = 0; i < 6; ++i, xVal += xStepSize, yVal += yStepSize) {
+			splineControlY.push_back(yVal);
+			splineControlX.push_back(xVal);
+			//std::cout << "Xval = "<< xVal << " Yval = " << yVal << '\n';
+		}
+		// setup intensity slider
+		splineIntensitySlider.set("Spline Intensity", 0, -yStepSize * MAX_SPLINE_CONTROL_PERC, yStepSize * MAX_SPLINE_CONTROL_PERC);
+	}
+
+	// PRIVATE Fields
+	
+	// Segment values
+	double start = 0;
+	double end = 1;
+	double startLevel = 0;
+	double endLevel = 1;
+
+	// Derived values
+	double totalLength;
+
+	// enum type of envelope segment
+	EnvelopeSegmentType type;
+	// spline control values
+	std::vector<double> splineControlX, splineControlY;
+
+	// constants
+	const int NUM_INTERNAL_CONTROL_SEGMENTS = 3;
+	const float MAX_SPLINE_CONTROL_PERC = 0.88888888; // so that control points don't put spline above/below target val
+
+};
+
+// TODO
+//class ADSRCurve {
+//	
+//	EnvelopeSegment attack, sustain, decay, release;
+//};
+//enum ADSRCurveType {
+//ADR, ADSR
+//};
 
 class ADSRVisualizer : public MIDIAnimationComponent {
 public:
@@ -27,76 +111,22 @@ public:
 	void draw();
 
 private:
+	EnvelopeSegment attackSegment;
 
-	// private mehotds
-	bool shouldChange();
-
-	void drawADSRTest();
-
-	// private fields
-	NoteADSRState testADSR;
-
-	//ofParameter<bool> testADSRToggle;
-	ofParameter<float> splineIntensitySlider;
-
-	
-	tk::spline splineAttack, splineDecay, splineRelease; // TODO unique spline for each ADSR segment
+	// display params
+	uint64_t colorTimeMS = 100; // in ms
+	uint64_t lastColorChange = 0;
 
 	int numPoints = 128;
 
 	std::vector<ofColor> colors;
-	
-	const double minF = -2.f;
-	const double maxF = 2.f;
-
-	std::vector<ofParameter<double>> splineControlX, splineControlY;
-	std::vector<double> scXDefaults 
-		//{ 0, .333, .666, 1 };
-		{ -.333f, 0.f, .333f, .666f, 1.f, 1.333f };//,  1.5f };
-	std::vector<double> scYDefaults 
-		//{ 0, .333, .666, 1 }; 
-		{ -.333f, 0.f, .333f, .666f, 1.f, 1.333f };//,  1.f };
-
-	uint64_t colorTimeMS = 100; // in ms
-	uint64_t lastColorChange = 0;
-
 	ofColor currentColor;
 
-	// display params
 	ofParameter<double> circleSize;
 	ofParameter<int> changeSpeed;
-
-
-	int minYToChange = 1, maxYToChange = scYDefaults.size() - 2, currentYToChange = minYToChange;
-
+	//int minYToChange = 1, maxYToChange = scYDefaults.size() - 2, currentYToChange = minYToChange;
+	
+	// private methods
+	bool shouldChange();
 
 };
-
-
-// TODO 
-//class EnvelopeParam {
-//public:
-//
-//	EnvelopeParam(double startLevel, double endLevel, std::vector<double> xControlPoints, std::vector<double> yControlPoints) : startLevel(startLevel), endLevel(endLevel), xControlPoints(xControlPoints), yControlPoints(yControlPoints)  {
-//
-//	}
-//
-//	EnvelopeParam() {
-//
-//	}
-//
-//	double startLevel, endLevel;
-//	std::vector<double> xControlPoints, yControlPoints;
-//	
-//	//EnvelopeParam attack, decay, sustain, release;
-//
-//
-//
-//	ADSR adsr;
-//	std::vector<ofParameter<double>> xPoints { -.5f, 0.f, .5f, 1.f,  1.5f };// ,
-//		//yPoints{ 0.f,  0.f, midpointLevel, adsr.aL, adsr.aL}
-//	
-//	/*void setup(ofxPanel& panel) {
-//		panel.add(textEntryField.setup());
-//	}*/
-//};
