4f709e5066f3dff76d30b95e9e22c6135e32a6a0
Ross Hoyt <rosshoytmusic@gmail.com>
2020-12-27 20:52:22 -0800

    Added GUI parameters to LFOVisualizer

--- of-midi-visualizer/src/LFOVisualizer.cpp
+++ of-midi-visualizer/src/LFOVisualizer.cpp
@@ -2,31 +2,32 @@
 
 LFOVisualizer::LFOVisualizer(std::string uid) : MIDIAnimationGUIComponent(uid), sineLFO() {}
 
-void LFOVisualizer::setupGUI()
-{
+void LFOVisualizer::setupGUI() {
+	gui.add(lfoTimeLength.set("LFO Time Length (MS)", lfoTimeLength, 4, 1000));
+	gui.add(numPoints.set("Number of Points", numPoints, 1 << 5, 1 << 11));
+	gui.add(circleSize.set("Circle Size", circleSize, 2, 100));
+	gui.add(pointLength.set("Point Length (MS)", pointLength, 10, 100));
+	gui.add(color);
 }
 
-void LFOVisualizer::setup()
-{
+void LFOVisualizer::setupParameterListeners() {
+	addParameterListener<double>(lfoTimeLength);
+	addParameterListener<int>(numPoints);
+	addParameterListener<double>(pointLength);
+	addParameterListener<double>(circleSize);
+}
+
+
+void LFOVisualizer::setup() {}
+
+void LFOVisualizer::update() {}
+
+void LFOVisualizer::draw() {
+	ofSetColor(color);
 	sineLFO.setTimeLength(lfoTimeLength);
-}
-
-void LFOVisualizer::update()
-{
-}
-
-void LFOVisualizer::draw()
-{
-	int numPoints = 128;
-	double pointLength = 10; // each point drawn will equal 10 MS
-	float circleSize = 20;
-	ofSetColor(ofColor::green);
 	for (int i = 0; i < numPoints; ++i) {
-		auto x = float(i) / float(numPoints) * animationWidth;
-		auto y = sineLFO.getValue(i * pointLength);
-
-		ofDrawCircle({ x, y}, circleSize);
-
-
+		float x = float(i) / float(numPoints);
+		float y = sineLFO.getValue(i * pointLength);
+		ofDrawCircle({ x * animationWidth, y * animationHeight}, circleSize);
 	}
 }
--- of-midi-visualizer/src/LFOVisualizer.h
+++ of-midi-visualizer/src/LFOVisualizer.h
@@ -2,20 +2,24 @@
 #include "MIDIAnimationGUIComponent.h"
 #include "LFO.h"
 
-class LFOVisualizer :
-	public MIDIAnimationGUIComponent
-{
+class LFOVisualizer : public MIDIAnimationGUIComponent {
 public:
 	LFOVisualizer(std::string uid = "LFOVisualizer");
 	// Inherited via GUIAnimationComponent
 	void setupGUI() override;
 	void setup() override;
+	void setupParameterListeners() override;
 	void update() override;
 	void draw() override;
 private:
 	SineLFO sineLFO;
 
+	// gui params
 	ofParameter<double> lfoTimeLength = 250;
-	
+	ofParameter<int> numPoints = 1024;
+	ofParameter<double> circleSize = 20;
+	ofParameter<double> pointLength = 10; // each point drawn will equal 10 MS
+	ofParameter<ofColor> color = ofColor::mediumSeaGreen;
 };
 
+	
